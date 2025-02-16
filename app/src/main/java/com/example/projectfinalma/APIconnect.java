84dec78bc846c4d9aaae71ca7b731010c4186875
husseinnajdi <husseinnajdi32@gmail.com>
2025-02-16 11:33:30 +0200

    Update APIconnect.java

--- app/src/main/java/com/example/projectfinalma/APIconnect.java
+++ app/src/main/java/com/example/projectfinalma/APIconnect.java
@@ -4,8 +4,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIconnect {
-    //private static final String BASE_URL = "http://10.0.2.2:3000/";
-    private static final String BASE_URL = "http://192.168.148.183:3000/";
+    private static final String BASE_URL = "http://10.0.2.2:3000/";
+    //private static final String BASE_URL = "http://192.168.148.183:3000/";
    // private static final String BASE_URL = "https://l5sftx2x-3000.uks1.devtunnels.ms/";
     private static Retrofit retrofit = null;
 
